1e8ae9f29f4e060f2fda3f1884399c3eb4d6a211
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2022-10-10 08:20:58 -0300

    feat: update main layouts look

---
+++ front/src/views/template/Centralized.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="d-flex vh-100 justify-center align-center" fluid>
+    <router-view />
+  </v-container>
+</template>
+<script>
+export default {
+  name: "CentralizedView",
+};
+</script>
+<style scoped>
+.vh-100 {
+  height: 100vh;
+}
+</style>
---
+++ front/src/views/template/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <SideBar />
+    <AppBar />
+
+    <v-main app>
+      <v-container class="px-5" style="min-height: 90vh" fluid>
+        <v-slide-x-reverse-transition mode="out-in">
+          <router-view />
+        </v-slide-x-reverse-transition>
+      </v-container>
+    </v-main>
+  </div>
+</template>
+<script>
+import SideBar from "@/components/SideBar";
+import AppBar from "@/components/AppBar";
+
+export default {
+  name: "DashboardView",
+
+  components: {
+    AppBar,
+    SideBar,
+  },
+};
+</script>

5810dae80ffef4ee5594ce42bac09f621ae46107
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2022-10-10 08:22:48 -0300

    refactor: update vuetify default colors

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -4,5 +4,22 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-
+  theme: {
+    themes: {
+      light: {
+        secondary: "#b0bec5",
+        danger: "#f44336",
+        warning: "#ff9800",
+        anchor: "#8c9eff",
+        appbar: "#34495e",
+        sidebar: "#34495e",
+      },
+      dark: {
+        secondary: "#b0bec5",
+        danger: "#f44336",
+        warning: "#ff9800",
+        anchor: "#8c9eff",
+      },
+    },
+  },
 });
